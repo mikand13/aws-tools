2c44863b70c806223883feef36012ab20806464f
mikand13 <mikkelsen.anders@gmail.com>
2017-09-01 11:35:21 +0200

    bundle exec fix

--- aws-cleaner.sh
+++ aws-cleaner.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -xl
 cd aws-ecr-cleaner
 bundle install --path ~/.gem
-bundle exec clean-ecs.rb 7
+bundle exec ruby clean-ecs.rb 7
--- aws-update-as-amis.sh
+++ aws-update-as-amis.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -xl
 cd aws-update-all-ecs-auto-scaling-groups
 bundle install --path ~/.gem
-bundle exec update-auto-scaling-groups.rb
+bundle exec ruby update-auto-scaling-groups.rb
